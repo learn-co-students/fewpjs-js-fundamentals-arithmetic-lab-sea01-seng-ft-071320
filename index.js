0d0fc47af8277c17d3fbd6ebb7ad6bf90136279c
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-01 16:34:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 62 / num1;
+
+const multiply = num1 * num2;
+const random = Math.random();
+
+const mod = 24 % 5;
+
+const max = Math.max(1, 15, 5, 20, 3);
